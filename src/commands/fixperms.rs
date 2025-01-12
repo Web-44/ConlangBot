37a8996ff5bfe1f74b11539b23b5328993caa882
Webbiii <26797607+Web-44@users.noreply.github.com>
2025-01-12 16:21:58 +0100

    Use the actual channel owner

--- src/commands/fixperms.rs
+++ src/commands/fixperms.rs
@@ -3,6 +3,7 @@ use crate::*;
 use serenity::all::{CommandInteraction, PermissionOverwrite, PermissionOverwriteType, Permissions};
 use serenity::builder::{CreateCommand, EditInteractionResponse};
 use serenity::client::Context;
+use crate::database::get_channel_by_id;
 
 pub fn register() -> CreateCommand {
     CreateCommand::new("fixperms")
@@ -14,16 +15,22 @@ pub async fn run(ctx: &Context, cmd: CommandInteraction) {
     let _ = cmd.defer_ephemeral(&ctx).await;
 
     let data = ctx.data.read().await;
+    let database_pool = data.get::<DatabasePoolKey>().unwrap();
     let profile = data.get::<Profile>().unwrap();
 
-    if let Ok(discord_channel) = cmd.channel_id.to_channel(&ctx).await {
-        let discord_channel = discord_channel.guild().unwrap();
-        let _ = discord_channel.create_permission(&ctx, hide_to_everyone!(profile.roles.everyone())).await;
-        let _ = discord_channel.create_permission(&ctx, user_owner!(cmd.user.id)).await;
-        let _ = cmd.edit_response(&ctx, EditInteractionResponse::new()
-            .content("Fixed permissions")).await;
+    if let Ok(channel) = get_channel_by_id(database_pool.clone(), cmd.channel_id).await {
+        if let Ok(discord_channel) = cmd.channel_id.to_channel(&ctx).await {
+            let discord_channel = discord_channel.guild().unwrap();
+            let _ = discord_channel.create_permission(&ctx, hide_to_everyone!(profile.roles.everyone())).await;
+            let _ = discord_channel.create_permission(&ctx, user_owner!(channel.owner)).await;
+            let _ = cmd.edit_response(&ctx, EditInteractionResponse::new()
+                .content("Fixed permissions")).await;
+        } else {
+            let _ = cmd.edit_response(&ctx, EditInteractionResponse::new()
+                .content("Failed to retrieve channel data")).await;
+        }
     } else {
         let _ = cmd.edit_response(&ctx, EditInteractionResponse::new()
-            .content("Failed to retrieve channel data")).await;
+            .content("Could not find channel data")).await;
     }
 }
